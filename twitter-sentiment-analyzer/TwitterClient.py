9439b9e7f890e08614b96b68f221769f12d520c2
Roman Lukomskyi <rlukomskyi@gmail.com>
2019-02-20 18:12:25 +0200

    Add basic TwitterClient implementation

---
+++ twitter-sentiment-analyzer/TwitterClient.py
@@ -0,0 +1,29 @@
+import tweepy
+import config
+
+from tweepy import OAuthHandler
+
+class TwitterClient(object):
+    def __init__(self):
+        try:
+            self.auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+            self.auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+            self.tweepyAPI = tweepy.API(self.auth)
+        except:
+            print("Error: Authentication Failed")
+
+    def get_tweets(self, query, count):
+        tweets = []
+        try:
+            fetched_tweets = self.tweepyAPI.search(q = query, count = count)
+            for tweet in fetched_tweets:
+                if tweet.retweet_count > 0:
+                    # event if tweet has retweets, append only once
+                    if tweet not in tweets:
+                        tweets.append(tweet)
+                else:
+                    tweets.append(tweet)
+            return tweets
+
+        except tweepy.TweepError as e:
+            print("Error : " + str(e))
