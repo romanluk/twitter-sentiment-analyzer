08c296b63fbae806db56aaa7ad51ba10be71bea1
Roman Lukomskyi <rlukomskyi@gmail.com>
2019-03-05 00:51:14 +0200

    Add /search-term endpoint

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, abort, request, jsonify
+import csv
+import sys
+from  twitter_sentiment_analyzer.TwitterClient import TwitterClient
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-    return "Hello world"
+@app.route('/search-terms', methods=['GET', 'POST'])
+def searchTerms():
+    if request.method == 'POST':
+        with open('terms.csv', 'a') as terms_file:
+            terms_writer = csv.writer(terms_file, delimiter=',')
+            term = request.form.get('term')
+            terms_writer.writerow([term])
+            return jsonify(success = True, message = 'Success', data = term), 200
+    elif request.method == 'GET':
+        pass
 
-if __name__ == "__main__":
+
+def setup():
+    prepareTwitterClient()
     app.run(debug = True)
+
+def prepareTwitterClient():
+    twitter_client = TwitterClient()
+
+setup()
