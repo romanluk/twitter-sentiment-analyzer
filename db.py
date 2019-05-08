7e4c067006e5fb4030a774f8b18fcc705f4b1e2e
Roman Lukomskyi <rlukomskyi@gmail.com>
2019-05-09 00:29:35 +0300

    Add basic firestore database class

---
+++ db.py
@@ -0,0 +1,27 @@
+import firebase_admin, entities
+from firebase_admin import credentials, firestore
+from entities import User, Dashboard, ReportPeriod
+
+class FirestoreDb(object):
+    def __init__(self):
+        cred = credentials.Certificate('./service_account_key.json')
+        firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
+        print("Firestore DB class initialized")
+
+    def add_user(self, uid):
+        self.db.collection(u'users').document(uid).set({})
+
+    def add_dashboard(self, user_id, dashboard):
+        dashboards_ref = self.db.collection(u'users').document(user_id).collection(u'dashboards')
+        dashboards_ref.add({
+            u'title' : dashboard.title,
+            u'search_term' : dashboard.search_term
+        })
+    
+    def add_period_data(self, user_id, dashboard_id, period_data):
+        dashboards_ref = self.db.collection(u'users').document(user_id).collection(u'dashboards')
+        dashboards_ref.document(dashboard_id).collection('periods').add({
+            u'start' : period_data.start,
+            u'end' : period_data.end
+        })
