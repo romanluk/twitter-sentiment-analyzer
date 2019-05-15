0353f4883cc4bbc209616dfd2d60a68e2f7a5c3f
Roman Lukomskyi <rlukomskyi@gmail.com>
2019-05-15 12:41:34 +0300

    Delete unneeded /search-terms route

--- app.py
+++ app.py
@@ -8,20 +8,6 @@ from entities import Dashboard
 
 app = Flask(__name__)
 
-@app.route('/search-terms', methods=['GET', 'POST'])
-def searchTerms():
-    if request.method == 'POST':
-        if not os.path.exists('data'):
-            os.makedirs('data')
-        with open('data/terms.csv', 'a+') as terms_file:
-            terms_writer = csv.writer(terms_file, delimiter=',')
-            userId = request.form.get('userId')
-            term = request.form.get('term')
-            terms_writer.writerow([userId, term])
-            return jsonify(success = True, message = 'Success', data = term), 200
-    elif request.method == 'GET':
-        pass
-
 @app.route('/dashboards', methods=['GET', 'POST'])
 def dashboards():
     if request.method == 'GET':
